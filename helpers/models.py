bd1cc821d3b079aa0e6f0a496ce02f64edfbcb38
Mawoka <mawoka@e.email>
2021-12-02 16:30:35 +0100

    Added/modified models

--- helpers/models.py
+++ helpers/models.py
@@ -10,12 +10,20 @@ class BaseUserCreate(BaseModel):
 
 
 class PublicUser(BaseModel):
+    public_collections: List[str] = []
     username: str
     id: str
     date_joined: str
 
 
+class PrivateUser(PublicUser):
+    private_collections: List[str] = []
+    email: str
+
+
 class UserInDB(BaseUserCreate):
+    public_collections: List[str] = []
+    private_collections: List[str] = []
     verified: bool = False
     things_created: int = 0
     id: str
@@ -35,19 +43,32 @@ class TokenData(BaseModel):
 class BaseFile(BaseModel):
     creation_date: str
     user_id: str
-    hashsum: str
+    hash: str
     file_id: str
     reported: bool = False
 
 
 class BaseInputThing(BaseModel):
+    category: str
     description: str
-    title: str
     file: str
-    pictures: List[str]
+    title: str
+    pictures: Optional[List[str]] = []
+    license: str
 
 
-class BaseThing(BaseInputThing):
+class PublicThing(BaseInputThing):
+    comment_count: int = 0
+    like_count: int = 0
+    make_count: int = 0
+    remixes: int = 0
     creation_date: str
     id: str
+    file: str
     user_id: str
+    downloads: int = 0
+
+
+class BaseThing(PublicThing):
+    hash: str
+
