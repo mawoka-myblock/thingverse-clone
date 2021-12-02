d0daa9a4dbfc07da4a569ccab8cbc78714550806
Mawoka <mawoka@e.email>
2021-12-02 16:33:30 +0100

    :bug: Fixed import-error

--- routers/things.py
+++ routers/things.py
@@ -14,7 +14,7 @@ from helpers.models import *
 import zipfile
 from helpers.security.verify import send_mail
 from helpers.security.auth import get_current_user, get_password_hash, get_user_from_id, verify_password, \
-    create_access_token, get_user_from_mail, get_user_from_username, authenticate_user, optional_current_user
+    create_access_token, get_user_from_mail, get_user_from_username, authenticate_user
 from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
