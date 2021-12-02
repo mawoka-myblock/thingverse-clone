92a6ecc830ef2868c1a11dbaa8bbe512d0db3d52
Mawoka <mawoka@e.email>
2021-12-02 16:29:46 +0100

    Added license-dict

--- helpers/config.py
+++ helpers/config.py
@@ -44,6 +44,21 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+license_dict = {
+    "CC0": "https://creativecommons.org/publicdomain/zero/1.0/",
+    "CC-BY": "https://creativecommons.org/licenses/by/4.0/",
+    "CC-BY-SA": "https://creativecommons.org/licenses/by-sa/4.0/",
+    "CC-BY-ND": "https://creativecommons.org/licenses/by-nd/4.0/",
+    "CC-BY-NC": "https://creativecommons.org/licenses/by-nc/4.0/",
+    "CC-BY-NC-SA": "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+    "CC-BY-NC-ND": "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+    "CC-BY-NC-SA-ND": "https://creativecommons.org/licenses/by-nc-sa-nd/4.0/",
+    "GPL": "https://www.gnu.org/licenses/gpl-3.0.en.html",
+    "MIT": "https://opensource.org/licenses/MIT",
+    "BSD": "https://opensource.org/licenses/BSD-3-Clause",
+    "LGPL": "https://www.gnu.org/licenses/lgpl-3.0.en.html",
+}
+
 minio = Minio(
     endpoint=settings.minio_url,
     access_key=settings.minio_access_key,
