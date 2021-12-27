99c5639c3e1010ad854effea0d2b4441eef0037a
Mawoka <mawoka@e.email>
2021-12-27 19:46:18 +0100

    Started on adding items to lists

--- frontend/src/routes/thing/[slug].svelte
+++ frontend/src/routes/thing/[slug].svelte
@@ -18,15 +18,31 @@
 <script>
 	export let thing;
 	export let user;
-	import { marked } from 'marked';
+	import { marked, options } from 'marked';
+	import {onMount} from "svelte"
 	import sanitizeHtml from 'sanitize-html';
 	import Navbar from '$lib/navbar.svelte';
 	import { DateTime } from 'luxon';
+	import { Circle } from 'svelte-loading-spinners';
+	import { goto } from '$app/navigation';
 	import 'swiper/css';
 	import { Swiper, SwiperSlide } from 'swiper/svelte';
-    import tippy from "sveltejs-tippy";
-	import {loggedin} from "$lib/stores"
+	import tippy from 'sveltejs-tippy';
+	import { loggedin } from '$lib/stores';
+	let selectedCol;
+	let createCol;
+	let url = ""
+	onMount(() => (url = window.location.href));
 	console.log($loggedin)
+	const getCollectionsFunc = async () => {
+		const res = await fetch(`${apiurl}/api/v1/collections/my`);
+		if (res.status !== 200) {
+			goto('/account/login?ref=', url);
+		}
+		return await res.json();
+	};
+
+	let getCollections = getCollectionsFunc()
 	const prettyfyDate = (date) => {
 		const dt = DateTime.fromISO(date);
 		let minute;
@@ -40,6 +56,29 @@
 		//const datum = date.substring(0, 10)
 		// return `${datum} um ${time}`
 	};
+
+	const createNewCollection = async () => {
+		const res = await fetch(`${apiurl}/api/v1/collections/create`, {
+			method: "POST",
+			body: JSON.stringify({name: createCol})
+		})
+		if (res.status === 401) {
+			goto('/account/login?ref=', url);
+		}
+		console.log("Created new Collection")
+		getCollections = getCollectionsFunc()
+	}
+	const addToCollection = async () => {
+		const res = await fetch(`${apiurl}/api/v1/collections/${selectedCol}/update?thing=${thing._id}`, {
+			method: "PUT",
+		})
+		if (res.status === 401) {
+			goto('/account/login?ref=', url);
+		}
+
+	}
+	loggedin.subscribe(value => {console.log("val", value)})
+	$: console.log(selectedCol)
 </script>
 
 <Navbar />
@@ -62,24 +101,67 @@
 				</Swiper>
 			</div>
 			<div class="pl-4">
-				<div class="grid grid-cols-1 divide-y leading-6 rounded-lg shadow-lg overflow-hidden border-slate-200 border-2">
+				<div
+					class="grid grid-cols-1 divide-y leading-6 rounded-lg shadow-lg overflow-hidden border-slate-200 border-2"
+				>
 					<p class="p-2">Likes: {thing.like_count}</p>
 					<p class="p-2">Comments: {thing.comment_count}</p>
 					<p class="p-2">Makes: {thing.make_count}</p>
 					<p class="p-2">Remixes: {thing.remixes}</p>
 					<p class="p-2">Downloads: {thing.downloads}</p>
-					<p class="p-2" use:tippy={{content: "DD.MM.YYYY at HH:MM"}}>Created on: {prettyfyDate(thing.creation_date)}</p>
+					<p class="p-2" use:tippy={{ content: 'DD.MM.YYYY at HH:MM' }}>
+						Created on: {prettyfyDate(thing.creation_date)}
+					</p>
 				</div>
 			</div>
 			<div>
-				<div class="grid grid-cols-1 pl-4">
-					<a class="btn" href={`${apiurl}/api/v1/cdn/download/${user.username}/${thing.file}`}
-						>Download</a
+				<div class="grid grid-cols-2 pl-4">
+					<a
+						class="btn col-span-2"
+						href={`${apiurl}/api/v1/cdn/download/${user.username}/${thing.file}`}>Download</a
 					>
+					<!-- {#if $loggedin} -->
+					{#if true}
+						{#await getCollections}
+							<div class="text-center flex justify-center">
+								<div>
+									<Circle size="1" unit="rem" />
+								</div>
+							</div>
+						{:then cols}
+							{#if cols.length === 0}
+								<div class="pt-8 col-span-2">
+									<form on:submit|preventDefault={createNewCollection} class="grid grid-cols-2 grid-rows-1 gap-2">
+										<input bind:value={createCol} type="text" placeholder="Whatever you like!" class="input input-bordered" />
+										<button type="submit" class="btn">Create!</button>
+									</form>
+									
+								</div>
+							{:else}
+							
+								<div class="pt-8 col-span-2">
+									<form on:submit|preventDefault={addToCollection} class="grid grid-cols-2 grid-rows-1 gap-2">
+										<select bind:value={selectedCol} class="">
+											<!-- <option disabled="disabled" selected="selected">Choose</option> -->
+											{#each cols as col}
+												<option value={col._id} class="text-black">{col.name}</option>
+											{/each}
+										</select>
+										<button type="submit" class="btn">Add!</button>
+									</form>
+									
+								</div>
+							{/if}
+						{/await}
+					{/if}
 				</div>
 			</div>
-			<div class="ml-4 rounded-lg shadow-lg overflow-hidden w-full h-full col-span-2 row-span-2 mt-8 border-slate-200 border-2">
-				<article class="prose w-full h-full ml-4">{@html sanitizeHtml(marked(thing.description))}</article>
+			<div
+				class="ml-4 rounded-lg shadow-lg overflow-hidden w-full h-full col-span-2 row-span-2 mt-8 border-slate-200 border-2"
+			>
+				<article class="prose w-full h-full ml-4">
+					{@html sanitizeHtml(marked(thing.description))}
+				</article>
 			</div>
 		</div>
 	</section>
